802b1bab43fa118ddf6fe60293049653361b2f97
Viktoria Filimonova <viktoryafv@gmail.com>
2021-02-14 00:05:58 +0200

    fdf

--- task-4.js
+++ task-4.js
@@ -1,16 +1,16 @@
 const button = {
-    add: document.querySelector("[data-action='decrement']"),
-     span: document.querySelector("#value"),
-    sub: document.querySelector("[data-action='increment']"), 
-  };
-  let counterValue = 0;
-  const decrement = () =>{
-  counterValue -=1;
-  span.textContent = counterValue;
-  };
-  const increment = () =>{
-  counterValue +=1;
-  span.textContent = counterValue;
-  };
-  button.span.addEventListener("click", decrement);
-  button.span.addEventListener("click", increment);
+  add: document.querySelector("[data-action='decrement']"),
+  sub: document.querySelector("[data-action='increment']"),
+  span: document.querySelector("#value"), 
+};
+let counterValue = 0;
+const decrement = () =>{
+counterValue -=1;
+button.span.textContent = counterValue;
+};
+const increment = () =>{
+counterValue +=1;
+button.span.textContent = counterValue;
+};
+button.add.addEventListener('click', decrement);
+button.sub.addEventListener('click', increment);
